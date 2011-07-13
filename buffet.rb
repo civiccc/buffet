30055e078410bbc79c2597e846b75585694267a8
Grant Mathews <grant@causes.com>
2011-07-13 14:22:11 -0700

    Move campfire to lib/buffet.
    Beginning of an effort to make this repository a Gem.

--- buffet.rb
+++ buffet.rb
@@ -4,6 +4,7 @@ require 'ftools'
 require 'benchmark'
 
 $LOAD_PATH.push(File.expand_path(File.dirname(__FILE__)))
+$LOAD_PATH.push(File.expand_path(File.join(File.dirname(__FILE__), "lib", "buffet")))
 
 require 'wopen3'
 require 'master'
