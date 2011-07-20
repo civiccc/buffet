0a8e70235a1488014353015a275e27f4594045d5
Grant Mathews <grant@causes.com>
2011-07-20 10:49:21 -0700

    Clean up and document buffet.rb.

--- lib/buffet/buffet.rb
+++ lib/buffet/buffet.rb
@@ -15,9 +15,22 @@ PID_FILE = "/tmp/buffet.pid"
 SETTINGS_FILE = File.expand_path('../../settings.yml', File.join(File.dirname(__FILE__)))
 
 module Buffet
+  # This is the core Buffet class. It uses Setup and Master to bring the working
+  # directory up to sync and to run tests remotely, respectively. It exposes 
+  # some helpful methods to give immediate information about the state of the 
+  # tests and other relevant information.
   class Buffet
+
+    ################
+    # CORE METHODS #
+    ################
+
+    # Initialize sets up preliminary data, and will clone the repository 
+    # specified in settings.yml into working-directory if necessary.
+    #
+    # Initialize will NOT begin testing.
     def initialize repo
-      @status = StatusMessage.new true
+      @status = StatusMessage.new
       @repo = repo
       @working_directory = './working-directory'
       @state = :not_running
@@ -34,63 +47,11 @@ module Buffet
       end
     end
 
-    def repo
-      @repo
-    end
-
-    def running?
-      @state != :not_running
-    end
-
-    def testing?
-      @state == :testing
-    end
-
-    def hosts
-      Settings.get['hosts']
-    end
-
-    def list_branches
-      Dir.chdir(@working_directory) do
-        `git branch -a`
-      end
-    end
-    memoize :list_branches
-
-    def num_tests
-      # This is RSpec specific.
-      `grep -r "  it" #{@working_directory}/spec/ | wc`.to_i
-    end
-    memoize :num_tests
-
-    def write_pid
-      File.open(PID_FILE, 'w') do |fh|
-        fh.write(Process.pid)
-      end
-    end
-
-    def clear_pid
-      if File.read(PID_FILE).to_i == Process.pid
-        File.delete(PID_FILE)
-      end
-    end
-
+    # Run is the core method of Buffet. It sets up and tests the working directory.
     def run(skip_setup=false)
-      if File.exists?(PID_FILE)
-        if `ps aux | grep buffet | grep -v grep | grep #{File.open(PID_FILE).read}`.length == 0
-          # Buffet isn't running, but the PID_FILE exists.
-          # Get rid of it.
-          FileUtils.rm(PID_FILE)
-        else
-          puts "Buffet is already running. Hold your horses."
-          exit 1
-        end
-      end
-
       @threads << Thread.new do
-        begin
-          write_pid
-      
+        ensure_only_one do
+          @status.set "Preliminary setup..."
           @state = :setup
           @setup = Setup.new @working_directory, hosts, @status, @repo
           @setup.run "master"
@@ -100,22 +61,62 @@ module Buffet
           @master.run
         
           @state = :not_running
-        ensure
-          clear_pid
         end
       end
     end
 
-    def wait_until_done
-      @threads.each do |thread|
-        thread.join
+    ###################### 
+    # ENSURE EXCLUSIVITY #
+    ###################### 
+
+    # Ensure that only one instance of the block passed in runs at any time,
+    # across the entire machine.
+    def ensure_only_one
+      # We ensure exclusivity by writing a file to /tmp, and checking to see if it
+      # exists before we start testing.
+      def write_pid
+        File.open(PID_FILE, 'w') do |fh|
+          fh.write(Process.pid)
+        end
+      end
+
+      def clear_pid
+        if File.read(PID_FILE).to_i == Process.pid
+          File.delete(PID_FILE)
+        end
+      end
+
+      if File.exists?(PID_FILE)
+        if `ps aux | grep buffet | grep -v grep | grep #{File.open(PID_FILE).read}`.length == 0
+          # Buffet isn't running, but the PID_FILE exists.
+          # Get rid of it.
+          FileUtils.rm(PID_FILE)
+        else
+          puts "Buffet is already running. Hold your horses."
+          return
+        end
+      end
+
+      begin
+        write_pid
+        yield
+      ensure
+        clear_pid
       end
     end
 
+    ##################
+    # TESTING STATUS #
+    ##################
+
+    # What is Buffet currently doing?
+    # This method is only meaningful when called from a separate thread then 
+    # the one running Buffet.
     def get_status
       @status
     end
 
+    # An array of failed test cases.
     def get_failures
       if @state == :testing
         @master.failures
@@ -123,5 +124,40 @@ module Buffet
         []
       end
     end
+
+    # The URL of the respository.
+    def repo
+      @repo
+    end
+
+    # Is Buffet running (where running is either testing or setting up)?
+    def running?
+      @state != :not_running
+    end
+
+    # Is Buffet testing?
+    def testing?
+      @state == :testing
+    end
+
+    # List all the hosts (found in settings.yml)
+    def hosts
+      Settings.get['hosts']
+    end
+
+    # List all branches (found by asking the working directory)
+    def list_branches
+      Dir.chdir(@working_directory) do
+        `git branch -a`
+      end
+    end
+    memoize :list_branches
+
+    # Count the number of tests. Uses a heuristic that is not 100% accurate.
+    def num_tests
+      # This is RSpec specific.
+      `grep -r "  it" #{@working_directory}/spec/ | wc`.to_i
+    end
+    memoize :num_tests
   end
 end
