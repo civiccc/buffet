85d3e71d60c2aac6b386b8c4c1f13cf14168ee3d
Grant Mathews <grant@causes.com>
2011-07-28 18:41:06 -0700

    Add --watch flag to CLI.
    When run with the --watch flag, Buffet will watch the repository
    specified in settings.yml for new commits. When one is found, Buffet
    will run the tests on that commit.

--- lib/buffet/cli.rb
+++ lib/buffet/cli.rb
@@ -7,6 +7,7 @@ $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/.."))
 # bug hunting.
 
 require 'buffet/buffet'
+require 'json'
 require 'buffet/settings'
 
 module Buffet
@@ -14,16 +15,44 @@ module Buffet
     def initialize
       # Set some initial settings. These may be changed by process_args.
       @branch = "master"
+      @watch = false
       #TODO: Annoying how one is yes and one is no.
       @skip_setup = false
       @dont_run_migrations = true
 
       process_args
 
-      puts "Running Buffet on branch #{@branch}."
+      if not @watch
+        puts "Running Buffet on branch #{@branch}."
 
-      buffet = Buffet.new(Settings.get["repository"], true)
-      buffet.run(@branch, {:skip_setup => @skip_setup, :dont_run_migrations => @dont_run_migrations})
+        buffet = Buffet.new(Settings.get["repository"], true)
+        buffet.run(@branch, {:skip_setup => @skip_setup, :dont_run_migrations => @dont_run_migrations})
+      else
+        puts "Watching #{Settings.get["repository"]}/master. Ctrl-C to quit."
+
+        watch
+      end
+    end
+
+    def watch
+      buffet_lock = Mutex.new
+
+      old_commit_message = ""
+      while true
+        api_call = "curl -u '#{Settings.get['github']['username']}/token:#{Settings.get['github']['token']}' 'https://github.com/api/v2/json/commits/list/#{Settings.get['github']['owner']}/#{Settings.get['github']['repository']}/master'"
+
+        commit_message = JSON.parse(`#{api_call}`)["commits"].first["message"]
+
+        if commit_message != old_commit_message and old_commit_message != ""
+          puts "New commit on master."
+
+          buffet = Buffet.new(Settings.get["repository"], true)
+          buffet.run(@branch, {:skip_setup => false, :dont_run_migrations => false})
+        end
+
+        old_commit_message = commit_message
+        sleep 2
+      end
     end
 
     def process_args
@@ -34,6 +63,11 @@ module Buffet
           puts "Usage: bundle exec ruby lib/buffet/cli.rb [flags]"
           puts ""
           puts "Flags"
+          puts "\t--watch"
+          puts "\t\tWatch the repo specified in settings.yml. This will not run"
+          puts "\t\tBuffet initially. Instead, the tests will be run every time" 
+          puts "\t\tsomeone pushes to master."
+          puts ""
           puts "\t--skip-setup"
           puts "\t\tSkip the setup step and just run tests."
           puts ""
@@ -47,6 +81,8 @@ module Buffet
           puts "\t\tDon't output anything while testing, except ./F."
 
           exit 0
+        elsif arg == "--watch"
+          @watch = true
         elsif arg == "--skip-setup"
           @skip_setup = true
         elsif arg == "--dont-run-migrations"
