f43ededa04022ca08c3cf01a11510256ffb4e1d0
Grant Mathews <grant@causes.com>
2011-08-08 14:12:19 -0700

    Rename buffet_worker to buffet-worker.
    Nice and consistent with the other bin/ scripts.

--- lib/buffet/master.rb
+++ lib/buffet/master.rb
@@ -95,7 +95,7 @@ module Buffet
         # Run worker on every host.
         threads = @hosts.map do |host|
           Thread.new do
-            `ssh buffet@#{host} 'cd ~/#{Settings.root_dir_name}/working-directory; RAILS_ENV=test bundle exec ruby ~/#{Settings.root_dir_name}/buffet/bin/buffet_worker #{server_addr}'`
+            `ssh buffet@#{host} 'cd ~/#{Settings.root_dir_name}/working-directory; RAILS_ENV=test bundle exec ruby ~/#{Settings.root_dir_name}/buffet/bin/buffet-worker #{server_addr}'`
 
             if $?.exitstatus != 0
               puts "Error on worker machine #{host}."
